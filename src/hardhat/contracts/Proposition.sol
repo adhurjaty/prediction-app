66718a7f300f3cde3ffe26375e492c11974f3ab3
Anthony Wong <anthony.chungku.wong@gmail.com>
2021-03-19 18:26:16 -0700

    Broke out base contract and interfaces

--- src/hardhat/contracts/Proposition.sol
+++ src/hardhat/contracts/Proposition.sol
@@ -1,12 +1,14 @@
 pragma solidity >=0.7.0 <0.8.0;
 
-contract Proposition {
 
+// TODO: Terminology struggles (bet, wager, pool)
+
+contract MembersOnly {
     mapping (address => bool) members;
-    mapping (address => uint256) member_wagers;
-    uint256 public wager_pool;
-    
-    // modifier to check if caller is a member
+
+    /**
+     * @dev Restricts modified functions to members-only
+     */
     modifier isMember() {
         // If the first argument of 'require' evaluates to 'false', execution terminates and all
         // changes to the state and to Ether balances are reverted.
@@ -16,28 +18,108 @@ contract Proposition {
         require(members[msg.sender], "Caller is not a member"); // or however you do a quick array search in Solidity...
         _;
     }
-    
-    /**
-     * @dev Set contract deployer as owner
-     */
-    constructor() {
-        members[msg.sender] = true;
-    }
 
     /**
-     * @dev Add member of group
-     * @param newMember address of new member
+     * @dev Adds a member to the contract
+     * @param newMember Address of new member
      */
     function addMember(address newMember) public isMember {
         members[newMember] = true;
     }
 
-    function addBet(uint256 wager) public isMember {
-        member_wagers[msg.sender] += wager;
-        wager_pool += wager;
+    /**
+     * @dev Checks account membership to the contract
+     * @param account Address of the account to be checked
+     */
+    function checkMembership(address account) public view returns (bool) {
+        return members[account];
+    }
+}
+
+// contract Bet {
+//     mapping (address => uint256) bets;
+//     uint256 public pool;
+
+//     /**
+//      * @dev Checks a bet to the pool
+//      */
+//     // function getMyBet() public view isMember returns (uint256) {
+//     //     return bets[msg.sender];
+//     // }
+// }
+
+// contract EqualBet is MembersOnly, Bet {
+//     /**
+//      * @dev Adds a bet to the pool
+//      */
+//     function wager() public isMember {
+//         bets[msg.sender] = 1;
+//         pool += 1;
+//     }
+// }
+
+/** 
+ * @title Base proposition contract
+ */
+contract Proposition is MembersOnly {
+
+    // mapping (address => bool) members;
+
+    mapping (address => uint256) bets;
+    uint256 public pool;
+
+    string public title;
+    uint public bet_closing_time;
+    uint public resolution_time;
+    
+    
+    /**
+     * @dev Set contract deployer as a member
+     */
+    constructor(string memory _title, uint _resolution_time, uint _bet_closing_time) {
+        members[msg.sender] = true;
+        title = _title;
+        resolution_time = _resolution_time;
+        bet_closing_time = _bet_closing_time;
+
     }
 
-    function getMyWager() public view isMember returns (uint256) {
-        return member_wagers[msg.sender];
+    /**
+     * @dev Adds a bet to the pool
+     * @param amount Amount to be wagered
+     */
+    function wager(uint256 amount) virtual public isMember {
+        bets[msg.sender] += amount;
+        pool += amount;
     }
+
+    /**
+     * @dev Checks a bet to the pool
+     */
+    function getMyBet() public view isMember returns (uint256) {
+        return bets[msg.sender];
+    }
+}
+
+contract EqualAnteProposition is Proposition {
+
+    constructor(string memory _title, uint _resolution_time, uint _bet_closing_time) Proposition(_title,_resolution_time,_bet_closing_time) { }
+
+    /**
+     * @dev Adds a bet to the pool
+     * @param amount Amount to be wagered
+     */
+    function wager(uint256 amount) override public isMember {
+        require(amount == 1, "Cannot wager more than 1 in an equal ante proposition");
+        wager();
+    }
+
+    /**
+     * @dev Adds a bet to the pool
+     */
+    function wager() public isMember {
+        bets[msg.sender] = 1;
+        pool += 1;
+    }
+
 }
