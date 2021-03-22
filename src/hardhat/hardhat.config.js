1b441e523de1a70a03057512655c749c5c5e402f
Anthony Wong <anthony.chungku.wong@gmail.com>
2021-03-22 01:34:23 -0700

    Added  hardhat task

--- src/hardhat/hardhat.config.js
+++ src/hardhat/hardhat.config.js
@@ -1,4 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async taskArgs => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
+
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+  });
 
 task("accounts", "Prints the list of accounts", async () => {
       const accounts = await ethers.getSigners();
