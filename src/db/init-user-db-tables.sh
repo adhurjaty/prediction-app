853f800cb89bff98a4444b0ece534116075c6948
Anthony Wong <anthony.chungku.wong@gmail.com>
2021-04-07 13:38:50 -0700

    Updated tables to reflect db diagram

--- src/db/init-user-db-tables.sh
+++ src/db/init-user-db-tables.sh
@@ -26,12 +26,22 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
         CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES groups (id)
     );
 
-    CREATE TABLE proposition (
-        proposition_id serial PRIMARY KEY,
+    CREATE TABLE propositions (
+        id serial PRIMARY KEY,
         group_id int NOT NULL,
         title VARCHAR (255) NOT NULL,
         description TEXT,
-        is_resolved BOOLEAN,
+        address VARCHAR(50),
+        result BOOLEAN,
         CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES groups (id)
     );
+
+    CREATE TABLE votes (
+        id serial PRIMARY KEY,
+        user_id int NOT NULL,
+        proposition_id int NOT NULL,
+        vote BOOLEAN,
+        CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id),
+        CONSTRAINT fk_proposition_id FOREIGN KEY (proposition_id) REFERENCES propositions (id)
+    )
 EOSQL
