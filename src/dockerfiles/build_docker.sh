8b04e0f0ba3a8331302c2f2ee124637b6def293d
Anthony Wong <anthony.chungku.wong@gmail.com>
2021-04-06 21:17:13 -0700

    Postgres docker instance with initial tables

--- src/dockerfiles/build_docker.sh
+++ src/dockerfiles/build_docker.sh
@@ -4,3 +4,4 @@ cd `dirname $0`
 # TODO: check output and fail 
 docker build -f Dockerfile.hardhat.server -t braggingrights/hardhat-server:latest ..
 docker build -f Dockerfile.webapp -t braggingrights/webapp:latest ..
+docker build -f Dockerfile.db -t braggingrights/postgres:latest ..
