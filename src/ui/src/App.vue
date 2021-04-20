eaff170d15df652f1b3f0503106a295b62956eff
Anil Dhurjaty <adhurjaty@gmail.com>
2021-04-19 20:31:29 -0700

    gets router working

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view></router-view>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,13 +18,25 @@ import ContractControl from './components/ContractControl.vue';
 export default class App extends Vue {}
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- src/ui/src/main.ts
+++ src/ui/src/main.ts
@@ -1,17 +1,19 @@
-// import { createApp } from 'vue'
+import { createApp } from 'vue'
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import './registerServiceWorker'
 import router from './router'
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    router,
-    template: '<App/>',
-    components: { App }
-  })
-// createApp(App).mount('#app')
+// /* eslint-disable no-new */
+// new Vue({
+//     el: '#app',
+//     router,
+//     template: '<App/>',
+//     components: { App }
+//   })
+createApp(App)
+    .use(router)
+    .mount('#app')
