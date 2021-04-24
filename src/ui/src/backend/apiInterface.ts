9a9c607eacaaf177a73b9efb048263e9e4771968
Anil Dhurjaty <adhurjaty@gmail.com>
2021-04-24 07:29:38 -0700

    create frontend to backend interface

---
+++ src/ui/src/backend/apiInterface.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000';
+
+export class OauthConfirmRequest {
+    public code : string = '';
+    public verifier : string = '';
+
+    constructor(init? : Partial<OauthConfirmRequest>) {
+        Object.assign(this, init);
+    }
+}
+
+export class OauthConfirmResponse {
+    public accessToken : string = '';
+
+    constructor(init? : Partial<OauthConfirmResponse>) {
+        Object.assign(this, init);
+    }
+}
+
+export async function authConfirm(request : OauthConfirmRequest) {
+    const resp = await axios.post(`${BASE_URL}/confirm`, request);
+    debugger;
+    return new OauthConfirmResponse({
+        accessToken: resp.data.AccessToken
+    });
+}
