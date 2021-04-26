c7f0b97e89cff4e39ab699af2a97819c72215f8e
Anthony Wong <anthony.chungku.wong@gmail.com>
2021-04-26 16:20:28 -0700

    Implemented tests for EqualAntePropositions

--- src/tests/test-eaproposition.ts
+++ src/tests/test-eaproposition.ts
@@ -1,14 +1,35 @@
 import { ethers } from "hardhat";
-import { Signer } from "ethers";
+import { Signer, ContractFactory, Contract } from "ethers";
 import { expect } from "chai";
 
 describe("EqualAnteProposition", function () {
-    let accounts: Signer[];
+    let commissioner: Signer, member1: Signer, member2: Signer, nonmember1: Signer;
+    let propositionFactory: ContractFactory;
+    let proposition: Contract;
 
     beforeEach(async function () {
-        accounts = await ethers.getSigners();
+        propositionFactory = await ethers.getContractFactory("EqualAnteProposition");
+        [commissioner, member1, member2, nonmember1] = await ethers.getSigners();
+
+        const date = Date.now();
+
+        proposition = await propositionFactory.deploy("Test Proposition" + date, date+360, date+720);
+        await proposition.connect(commissioner).addMember(await member1.getAddress());
     });
 
-    it("Should ...", async function () { expect.fail("Test not implemented"); });
+    it("Should allow a member to wager without specifying a value", async function () { 
+        proposition.connect(member1)['wager()']();
+        expect(await proposition.connect(member1).getMyBet()).to.equal(1);
+    });
+
+    it("Should allow a member to wager exactly 1", async function () { 
+        proposition.connect(member1)['wager(uint256)'](1);
+        expect(await proposition.connect(member1).getMyBet()).to.equal(1);
+    });
+
+    it("Should prevent a member from wagering more than 1", async function () { 
+        await(expect(proposition.connect(member1)['wager(uint256)'](1000)).to.be.reverted);
+    });
+    // it("Should ...", async function () { expect.fail("Test not implemented"); });
 
 });
