b5130922c682d78181d6bd24da50bed9bda55e8e
Anthony Wong <anthony.chungku.wong@gmail.com>
2021-04-26 16:21:04 -0700

    Added failures for unimplemented tests

--- src/tests/test-proposition.ts
+++ src/tests/test-proposition.ts
@@ -6,25 +6,32 @@ describe("Proposition", function () {
     let commissioner: Signer, member1: Signer, member2: Signer, nonmember1: Signer;
     let propositionFactory: ContractFactory;
     let proposition: Contract;
+    let title: string;
 
     before(async function () {
         propositionFactory = await ethers.getContractFactory("Proposition");
         [commissioner, member1, member2, nonmember1] = await ethers.getSigners();
 
         const date = Date.now();
+        title = "Test Proposition" + date;
 
-        proposition = await propositionFactory.deploy("Test Proposition" + date, date+360, date+720);
+        proposition = await propositionFactory.deploy(title, date+360, date+720);
     });
 
-    it("Should deploy a proposition", async function () { });
-    it("Should allow someone to query the title", async function () { });
-    it("Should allow someone to query the current pool", async function () { });
+    it("Should allow anyone to deploy a proposition", async function () { expect.fail("Test not implemented"); });
+
+    it("Should allow anyone to query the proposition title", async function () { 
+        expect(await proposition.connect(nonmember1).title()).to.equal(title);
+    });
+
+    it("Should allow anyone to query the current pool", async function () { expect.fail("Test not implemented"); });
     // it("Should ...", async function () { expect.fail("Test not implemented"); });
 
     describe("Commissioner", function () {
         it("Should prevent a commissioner from adding themselves as a member", async function () { 
-            await(expect(proposition.connect(commissioner).addMember(commissioner)).to.be.reverted);
+            expect(await proposition.connect(commissioner).addMember(await commissioner.getAddress())).to.be.reverted;
         });
+
         it("Should allow a commissioner to add members", async function () { 
             expect.fail("Test not implemented");
         });
@@ -32,9 +39,9 @@ describe("Proposition", function () {
     });
     
     describe("MembersOnly", function () {
-        it("Should allow a member to place a bet", async function () {});
-        it("Should prevent a non-member from placing a bet", async function () {});
-        it("Should allow a member to check their bet amount", async function () { });
+        it("Should allow a member to place a bet", async function () {expect.fail("Test not implemented"); });
+        it("Should prevent a non-member from placing a bet", async function () {expect.fail("Test not implemented"); });
+        it("Should allow a member to check their bet amount", async function () { expect.fail("Test not implemented"); });
         // it("Should ...", async function () { expect.fail("Test not implemented"); });
     });
 });
