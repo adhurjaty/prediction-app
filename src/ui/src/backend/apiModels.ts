ca46b490efd610f93cbc24d10ab3e05352dcbe00
Anil Dhurjaty <adhurjaty@gmail.com>
2021-07-06 20:36:42 -0700

    fixes rebase conflicts

--- src/ui/src/backend/apiModels.ts
+++ src/ui/src/backend/apiModels.ts
@@ -1,7 +1,10 @@
 export class Group {
+    public id: string = '';
     public name: string = '';
     public accuracy?: number;
     public betsMade: number = 0;
+    public betsAvailable?: number;
+    public color?: string;
 
     constructor(init?: Partial<Group>) {
         Object.assign(this, init);
