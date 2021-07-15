bf1e3af356838f953a1c71f8a834b0920ef30e14
Anil Dhurjaty <adhurjaty@gmail.com>
2021-07-14 19:15:00 -0700

    goes back to async db calls

--- src/api/WebApi/Models/Group.cs
+++ src/api/WebApi/Models/Group.cs
@@ -54,24 +54,16 @@ namespace WebApi
         public override async Task LoadReferences(IDatabaseInterface db, 
             CancellationToken token = default)
         {
-            // bring this back when I figure out multiple active results setting for postgres
-            // await Task.WhenAll(UserGroups.Select(ug => 
-            //     db.LoadReferencesAsync(ug, token: token)));
-            foreach (var ug in UserGroups)
-            {
-                await db.LoadReferences(ug, token: token);
-            }
+            await Task.WhenAll(UserGroups.Select(ug => 
+                db.LoadReferences(ug, token: token)));
         }
 
-        private SemaphoreSlim _sem = new SemaphoreSlim(1);
         private async Task<Result<UserGroup[]>> ApplyToUserGroups(DbModel group,
             Func<UserGroup, Task> fn)
         {
             return await UserGroups.Select(async userGroup => 
             {
-                await _sem.WaitAsync();
                 await fn(userGroup);
-                _sem.Release();
                 return Result<AppUser>.Succeeded(userGroup);
             }).Aggregate();
         }
