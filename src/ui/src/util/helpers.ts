c8eabf52064c998dd1560663b0f4508aee46f3bc
Anil Dhurjaty <adhurjaty@gmail.com>
2021-08-06 17:45:24 -0700

    makes redirect decorator

--- src/ui/src/util/helpers.ts
+++ src/ui/src/util/helpers.ts
@@ -32,3 +32,24 @@ export function redirectToLoginOnError<T>(fn: () => Promise<T>): () => Promise<T
         }
     }
 }
+
+export function redirectToLoginOnErrorDec<T>(
+    target: Object,
+    key: string | symbol,
+    descriptor: PropertyDescriptor
+    )
+{
+    const fn = descriptor.value;
+    descriptor.value = async function (...args: any[]) {
+        try {
+            return await fn.apply(this, args);
+        } catch (e) {
+            if (e instanceof Error && e.message.includes('Unauthorized')) {
+                window.location.href = `/login?origin=${window.location.pathname}`;
+                return null;
+            }
+            throw e;
+        }
+    };
+    return descriptor;
+}
