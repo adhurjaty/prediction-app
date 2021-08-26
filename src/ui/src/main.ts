dc593d4ce1e6c09ad2a024686f4543cfac41a6ed
Anil Dhurjaty <adhurjaty@gmail.com>
2021-08-26 08:28:44 -0700

    starts restructure

--- src/ui/src/main.ts
+++ src/ui/src/main.ts
@@ -4,20 +4,22 @@ import './registerServiceWorker'
 import router from './router'
 import BackButton from './components/BackButton.vue';
 import Friend from './components/Friend.vue';
-//import VueRouter from 'vue-router'
-//import Vue from 'vue'
 
-// Vue.config.productionTip = false;
 
-// /* eslint-disable no-new */
-// new Vue({
-//     el: '#app',
-//     router,
-//     template: '<App/>',
-//     components: { App }
-//   })
-createApp(App)
-    .component('BackButton', BackButton)
-    .component('Friend', Friend)
-    .use(router)
-    .mount('#app')
+export class AppModule {
+
+    constructor() {
+
+        this.bootstrap();
+    }
+
+    private bootstrap(): void {
+        createApp(App)
+            .component('BackButton', BackButton)
+            .component('Friend', Friend)
+            .use(router)
+            .mount('#app')
+    }
+}
+
+new AppModule();
