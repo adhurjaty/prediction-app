f9f1e2580b38f8fa9bd5d1b62d4ad375d8c267ca
Anil Dhurjaty <adhurjaty@gmail.com>
2021-09-06 17:15:31 -0700

    gets injection working

--- src/ui/src/oauth/commands/confirmCommand.ts
+++ src/ui/src/oauth/commands/confirmCommand.ts
@@ -1,7 +1,8 @@
+import 'reflect-metadata';
 import { TOKEN_KEY, VERIFIER_KEY } from "@/util/constants";
 import { ILocalStorage } from "@/util/localStorage";
 import { ILocationBrowser } from "@/util/locationBrowser";
-import { inject, injectable } from "inversify-props";
+import { cid, container, inject, injectable } from "inversify-props";
 import { OauthConfirmRequest } from "../models";
 import { IOauthConfirmQuery } from "../queries/oauthConfirmQuery";
 
@@ -12,13 +13,15 @@ export interface IConfirmCommand {
 @injectable()
 export class ConfirmCommand implements IConfirmCommand {
     @inject() localStorage: ILocalStorage
-    @inject() location: ILocationBrowser;
-    @inject() confirmQuery: IOauthConfirmQuery;
+    // don't know why @inject won't work, but this works
+    // TODO: figure this out
+    private confirmQuery = container.get<IOauthConfirmQuery>(cid.IOauthConfirmQuery);
+    private location = container.get<ILocationBrowser>(cid.ILocationBrowser);
 
     public async execute(code: string, state: string): Promise<void> {
         const stateParams = new URLSearchParams(state);
         const origin = stateParams.get('origin');
-        const verifier = window.localStorage.getItem(VERIFIER_KEY);
+        const verifier = this.localStorage.getItem(VERIFIER_KEY);
 
         if(!verifier) {
             throw new Error('ERROR, could not get verifier from local storage');
