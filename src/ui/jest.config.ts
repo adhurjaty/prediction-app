0e5fb85479ba7189b85f0a00e84c72d9a67292bd
Daniel McLeod <danielmcleod@Daniels-MacBook-Pro.local>
2021-09-12 18:49:29 -0700

    installing more packages for jest

--- src/ui/jest.config.ts
+++ src/ui/jest.config.ts
@@ -1,19 +1,23 @@
-module.exports = {
-    roots: [
-      "<rootDir>/src"
-    ],
-    moduleFileExtensions: [
-        "ts",
-        "js",
-        "vue",
-        "json"
-    ],
-    testMatch: [
-      "**/__tests__/**/*.+(ts|tsx|js)",
-      "**/?(*.)+(spec|test).+(ts|tsx|js)"
-    ],
-    transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest",
-        "^.+\\.(vue)$": "vue-jest"
-    },
-  }
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  roots: [
+    "<rootDir>"
+  ],
+  moduleFileExtensions: [
+      "ts",
+      "js",
+      "vue",
+      "json"
+  ],
+  testMatch: [
+    "**/tests/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+      "^.+\\.(ts|tsx)$": "ts-jest",
+      "^.+\\.(vue)$": "vue-jest"
+  },
+}
+
+export default config;
