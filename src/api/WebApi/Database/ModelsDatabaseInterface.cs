a7c382c862d5fdce6312b84863665104e7874cad
Anil Dhurjaty <adhurjaty@gmail.com>
2021-10-24 00:38:55 -0700

    creates strategy database interface

--- src/api/WebApi/Database/ModelsDatabaseInterface.cs
+++ src/api/WebApi/Database/ModelsDatabaseInterface.cs
@@ -1,3 +1,5 @@
+using System.Threading;
+using System.Threading.Tasks;
 using Infrastructure;
 using ServiceStack.Data;
 
@@ -5,9 +7,36 @@ namespace WebApi
 {
     public class ModelsDatbaseInterface : DatabaseInterface, IDatabaseInterface
     {
-        public ModelsDatbaseInterface(IDbConnectionFactory dbFactory) : base(dbFactory)
+        private readonly IDbStrategyFactory _strategyFactory;
+
+        public ModelsDatbaseInterface(IDbConnectionFactory dbFactory, 
+            IDbStrategyFactory strategyFactory) : base(dbFactory)
         {
-            
+            _strategyFactory = strategyFactory;
+        }
+
+        public new async Task<Result> LoadReferences<T>(T model, CancellationToken token = default)
+        {
+            var strategy = _strategyFactory.Get<T>();
+            return await strategy.LoadReferences(model, token);
+        }
+
+        public new async Task<Result<long>> Insert<T>(T model, CancellationToken token = default)
+        {
+            var strategy = _strategyFactory.Get<T>();
+            return await strategy.Insert(model, token);
+        }
+
+        public new async Task<Result<int>> Update<T>(T model, CancellationToken token = default)
+        {
+            var strategy = _strategyFactory.Get<T>();
+            return await strategy.Update(model, token);
+        }
+
+        public new async Task<Result<int>> Delete<T>(T model, CancellationToken token = default)
+        {
+            var strategy = _strategyFactory.Get<T>();
+            return await strategy.Delete(model, token);
         }
     }
 }
