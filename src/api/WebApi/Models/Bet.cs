ef62f18b6c926b52ce69b4cfc380a90f560bf16b
Anil Dhurjaty <adhurjaty@gmail.com>
2021-10-24 01:14:52 -0700

    adds default strategy

--- src/api/WebApi/Models/Bet.cs
+++ src/api/WebApi/Models/Bet.cs
@@ -16,20 +16,5 @@ namespace WebApi
         public string Description { get; set; }
         public string Address { get; set; }
         public bool? Result { get; set; }
-
-        public override async Task<Result<DbModel>> Delete(IDatabaseInterface db, CancellationToken token = default)
-        {
-            return await Delete<Bet>(db, token);
-        }
-
-        public override async Task<Result<DbModel>> Insert(IDatabaseInterface db, CancellationToken token = default)
-        {
-            return await Insert<Bet>(db, token);
-        }
-
-        public override async Task<Result<DbModel>> Update(IDatabaseInterface db, CancellationToken token = default)
-        {
-            return await Update<Bet>(db, token);
-        }
     }
 }
