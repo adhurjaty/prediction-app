80d3087c6e37e126f440341d33ede9cf280b8bf0
Anil Dhurjaty <adhurjaty@gmail.com>
2021-10-26 21:58:27 -0700

    fixes oauth api errors

--- src/ui/src/app.types.ts
+++ src/ui/src/app.types.ts
@@ -1,8 +1,5 @@
-import { TYPES as OAUTH_TYPES } from './oauth/oauth.types'
 
 export const TYPES = {
     LOCAL_STORAGE: 'LOCAL_STORAGE',
     LOCATION_BROWSER: 'LOCATION_BROWSER',
-
-    ...OAUTH_TYPES
 }
