4dcb20cdb2f7ab48500e1a1b3985e59e3e5d86dd
Anil Dhurjaty <adhurjaty@gmail.com>
2022-07-25 15:14:01 +0200

    fix nextjs env variables not showing up

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,9 +1,16 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
-module.exports = withPWA({
-	pwa: {
-		dest: 'public',
-		runtimeCaching,
-    },
-})
+module.exports = {
+    ...withPWA({
+        pwa: {
+            dest: 'public',
+            runtimeCaching,
+        },
+    }),
+    env: {
+        FLOW_ACCESS_NODE: process.env.FLOW_ACCESS_NODE,
+        FLOW_DISCOVERY_WALLET: process.env.FLOW_DISCOVERY_WALLET,
+        FLOW_DELPHAI_ADDRESS: process.env.FLOW_DELPHAI_ADDRESS
+    }
+}
