c626bf6865f2aca13970c4e15fe8b1ed81651269
Anil Dhurjaty <adhurjaty@gmail.com>
2022-08-10 16:18:35 +0200

    fix node interface error bug

--- ui/utils/nodeInterface.ts
+++ ui/utils/nodeInterface.ts
@@ -1,34 +1,36 @@
 import { errAsync, okAsync, Result, ResultAsync } from "neverthrow";
 
 export function fetchModel<T>(url: string, signal?: AbortSignal): ResultAsync<T, string> {
-    return ResultAsync.fromPromise(
-            fetch(url, { signal }).then(res => res.json()),
-            e => e as string)
-        .map(json => json.result as T);
+    return responseToResult<T>(fetch(url, { signal }));
 }
 
 export function postModel<T>(url: string, model: any): ResultAsync<T, string> {
-    return ResultAsync.fromPromise(
+    return responseToResult<T>(
         fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(model),
-        }).then(res => res.json()),
-        e => e as string)
-    .map(json => json.result as T);
+        }));
 }
 
 export function putModel<T>(url: string, model: any): ResultAsync<T, string> {
-    return ResultAsync.fromPromise(
+    return responseToResult<T>(
         fetch(url, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(model),
-        }).then(res => res.json()),
-        e => e as string)
-    .map(json => json.result as T);
+        }));
+}
+
+function responseToResult<T>(response: Promise<Response>): ResultAsync<T, string> {
+    return ResultAsync.fromPromise(
+        response.then(res => res.json()),
+        e => (e as any | undefined)?.message as string | undefined || e as string
+    ).andThen(({ result, error }) => {
+        return error ? errAsync(error as string) : okAsync(result as T);
+    });
 }
