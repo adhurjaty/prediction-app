0c4ca7994f3553694ee8062493d7daea734c13a6
Anil Dhurjaty <adhurjaty@gmail.com>
2022-08-23 18:49:55 +0200

    move to .net6.0

--- api/Infrastructure/Extensions/LinqExtensions.cs
+++ api/Infrastructure/Extensions/LinqExtensions.cs
@@ -31,30 +31,6 @@ namespace Infrastructure
             return intersection;
         }
 
-        // code from MoreLinq https://github.com/morelinq/MoreLINQ/blob/master/MoreLinq/DistinctBy.cs
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
-            Func<TSource, TKey> keySelector)
-        {
-            return source.DistinctBy(keySelector, null);
-        }
-
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
-            Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer)
-        {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
-
-            return _(); IEnumerable<TSource> _()
-            {
-                var knownKeys = new HashSet<TKey>(comparer);
-                foreach (var element in source)
-                {
-                    if (knownKeys.Add(keySelector(element)))
-                        yield return element;
-                }
-            }
-        }
-
         /// <summary>
         /// Returns a list of lists of decreasing length, removing the first element in 
         /// each subsequent list. E.g. [1, 2, 3, 4] -> [[1, 2, 3, 4], [2, 3, 4], [3, 4]]
